1559672babb5d2e0587720b529f8b0980160b1f1
EliasAlmen <almen.elias@gmail.com>
2023-03-04 08:03:31 +0100

    SQL + TOOLS + DataContext

---
+++ EC05_C-sharp-EFC-DI-HOST-MVVM-WPF/Contexts/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC05_C_sharp_EFC_DI_HOST_MVVM_WPF.Contexts
+{
+    class DataContext : DbContext
+    {
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
